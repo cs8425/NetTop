24af3acfb4a51bbfe53ba147d15fda8a7315f749
cs8425 <cs8425@gmail.com>
2016-05-06 06:56:13 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# nettop
+a simple command line Bandwidth Monitor by parse `/proc/net/dev` to calculator.
+
+## Build
+```
+$ go build -o nettop nettop.go
+```
+
+## Usage
+```
+Usage of nettop:
+  -c uint
+    	count (0 == unlimit)
+  -i string
+    	interface (default "*")
+  -t float
+    	update time(s) (default 2)
+  -v int
+    	verbosity (default 2)
+
+```
+
+
+## LICENSE - MIT
+Copyright (C) 2016 cs8425
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
---
+++ nettop.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+
+	"time"
+	"runtime"
+	"flag"
+	"log"
+	"fmt"
+
+	"os"
+	"bufio"
+	"strings"
+	"strconv"
+)
+
+var T = flag.Float64("t", 2, "update time(s)")
+var C = flag.Uint("c", 0, "count (0 == unlimit)")
+var Inter = flag.String("i", "*", "interface")
+
+var verbosity = flag.Int("v", 2, "verbosity")
+
+type NetStat struct {
+	Dev []string
+	Stat map[string]*DevStat
+}
+
+type DevStat struct {
+	Name  string
+	Rx    uint64
+	Tx    uint64
+}
+
+func ReadLines(filename string) ([]string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return []string{""}, err
+	}
+	defer f.Close()
+
+	var ret []string
+
+	r := bufio.NewReader(f)
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ret = append(ret, strings.Trim(line, "\n"))
+	}
+	return ret, nil
+}
+
+func getInfo() (ret NetStat) {
+
+	lines, _ := ReadLines("/proc/net/dev")
+
+	ret.Dev = make([]string, 0)
+	ret.Stat = make(map[string]*DevStat)
+
+	for _, line := range lines {
+		fields := strings.Split(line, ":")
+		if len(fields) < 2 {
+			continue
+		}
+		key := strings.TrimSpace(fields[0])
+		value := strings.Fields(strings.TrimSpace(fields[1]))
+
+//		Vlogln(5, key, value)
+
+		if *Inter != "*" && *Inter != key {
+			continue
+		}
+
+		c := new(DevStat)
+//		c := DevStat{}
+		c.Name = key
+		r, err := strconv.ParseInt(value[0], 10, 64)
+		if err != nil {
+			Vlogln(4, key, "Rx", value[0], err)
+			break
+		}
+		c.Rx = uint64(r)
+
+		t, err := strconv.ParseInt(value[8], 10, 64)
+		if err != nil {
+			Vlogln(4, key, "Tx", value[8], err)
+			break
+		}
+		c.Tx = uint64(t)
+
+		ret.Dev = append(ret.Dev, key)
+		ret.Stat[key] = c
+	}
+
+	return
+}
+
+func main() {
+	log.SetFlags(log.Ldate|log.Ltime)
+	flag.Parse()
+
+//	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(1)
+
+	var stat0 NetStat
+	var stat1 NetStat
+	var delta NetStat
+	delta.Dev = make([]string, 0)
+	delta.Stat = make(map[string]*DevStat)
+
+	i := *C
+	if i > 0 {
+		i += 1
+	}
+
+	if *T < 0.01 {
+		*T = 0.01
+	}
+
+	start := time.Now()
+	elapsed := time.Since(start)
+	for {
+
+		elapsed = time.Since(start)
+		stat1 = getInfo()
+//		Vlogln(5, stat0)
+		for _, value := range stat1.Dev {
+			t0, ok := stat0.Stat[value]
+//			fmt.Println("k:", key, " v:", value, ok)
+			if ok {
+				dev, ok := delta.Stat[value]
+				if !ok {
+					delta.Stat[value] = new(DevStat)
+					dev = delta.Stat[value]
+					delta.Dev = append(delta.Dev, value)
+				}
+				t1 := stat1.Stat[value]
+				dev.Rx = t1.Rx - t0.Rx
+				dev.Tx = t1.Tx - t0.Tx
+			}
+		}
+		stat0 = stat1
+		for _, iface := range delta.Dev {
+			stat := delta.Stat[iface]
+			fmt.Printf("BW:\t%v\t%v\t%v\n", iface, Vsize(stat.Rx, *T), Vsize(stat.Tx, *T))
+		}
+		//elapsed := time.Since(start)
+		Vlogf(5, "[delta] %s", elapsed)
+		start = time.Now()
+
+		i -= 1
+		if i == 0 {
+			break
+		}
+
+		time.Sleep(time.Duration(*T * 1000) * time.Millisecond)
+
+	}
+}
+
+func Vsize(bytes uint64, delta float64) (ret string){
+	var tmp float64 = float64(bytes)/delta
+	var s string = ""
+
+	bytes = uint64(tmp)
+
+	switch {
+	case bytes < uint64(2<<9):
+
+	case bytes < uint64(2<<19):
+		tmp = tmp/float64(2<<9)
+		s = "K"
+
+	case bytes < uint64(2<<29):
+		tmp = tmp/float64(2<<19)
+		s = "M"
+
+	case bytes < uint64(2<<39):
+		tmp = tmp/float64(2<<29)
+		s = "G"
+
+	case bytes < uint64(2<<49):
+		tmp = tmp/float64(2<<39)
+		s = "T"
+
+	}
+	ret = fmt.Sprintf("%0.2f %sB/s", tmp, s)
+	return
+}
+
+func Vlogf(level int, format string, v ...interface{}) {
+	if level <= *verbosity {
+		log.Printf(format, v...)
+//		fmt.Printf(format, v...)
+	}
+}
+func Vlog(level int, v ...interface{}) {
+	if level <= *verbosity {
+		log.Print(v...)
+//		fmt.Print(v...)
+	}
+}
+func Vlogln(level int, v ...interface{}) {
+	if level <= *verbosity {
+		log.Println(v...)
+//		fmt.Println(v...)
+	}
+}
+
+
+
